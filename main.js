b85cb024baf381b4067c44ad6d601c4e551575da
Dela Ricch <DelaRicch@gmail.com>
2022-07-14 21:52:08 +0000

    Created a simple URL Shortener

---
+++ main.js
@@ -0,0 +1,44 @@
+// Information to reach API
+const apiKey = 'fdde75b736b740d19a3d95f5e9572205';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok == true) {
+      return response.json()
+    } throw new Error ('Request failed!')
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+      renderResponse(jsonResponse)
+  })
+
+  document.querySelector('#input').value = ''
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
